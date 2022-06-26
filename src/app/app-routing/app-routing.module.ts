ebc1898870cd8977f8fd617ecec9c7162842a7e5
omar1218 <omar_1218@ymail.com>
2022-06-26 02:58:45 -0400

    assignment 2.7

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -7,22 +7,28 @@ import { HomeComponent } from '../components/home/home.component';
 import { PlaceholderComponent } from '../components/placeholder/placeholder.component';
 import { UserDetailsComponent } from '../components/user-details/user-details.component';
 import { UsersComponent } from '../components/users/users.component';
+import { AuthGuard } from '../guards/auth/auth.guard';
 
 
 const appRoutes : Routes = [
   { path : 'home' , component:HomeComponent} ,
   { path : 'about' , component:AboutComponent} ,
-  { path : 'blog' , component:BlogComponent} ,
-  { path : 'users' , component:UsersComponent , children:[
-    { path : ':id' , component:UserDetailsComponent},
-    { path :'' ,component:PlaceholderComponent }
-  ] },
+  { path : 'blog' , component:BlogComponent , canActivate: [AuthGuard]} ,
+  { path : 'users' , 
+        component:UsersComponent , 
+        canActivateChild:[AuthGuard],
+        children:[
+          { path : ':id' , component:UserDetailsComponent},
+          { path :'' ,component:PlaceholderComponent }
+      ] 
+  },
   { path : '**' , redirectTo:('/home'),pathMatch:'full'}
 
 ];
 
 @NgModule({
   declarations: [],
+  providers: [AuthGuard],
   imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
